dbf0d111f0733482f15e75bbcf2202888078309d
Vimal  Jain <vimaljain@Vimals-MacBook-Air.local>
2025-01-24 09:14:07 +0530

    add hero section

---
+++ src/common/Header.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import logo from "../assets/images/png/logo.png";
+import discordIcon from "../assets/images/svg/discord.svg";
+
+// Navigation Links Array
+const navLinks = [
+    { name: "About", href: "#about" },
+    { name: "Presale $GILD", href: "#presale" },
+    { name: "Staking $GILD", href: "#staking" },
+    { name: "$GILD Nodes", href: "#nodes" },
+];
+
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return (
+        <header className="bg-black/80">
+            {/* Main content wrapper */}
+            <div className={`container flex items-center justify-between ${isMenuOpen ? 'hidden' : ''}`}>
+                <div className="flex items-center py-4">
+                    <img src={logo} alt="Gilded Games Logo" className="md:w-[286px] md:h-[57px] w-[200px] h-[45px]" />
+                </div>
+
+                {/* Main Navigation */}
+                <nav className="hidden lg:flex xl:gap-10 gap-4 text-sm">
+                    {navLinks.map((link, index) => (
+                        <a
+                            key={index}
+                            href={link.href}
+                            className="hover:font-bold text-white text-[22px] transition-all ease-in-out duration-500"
+                        >
+                            {link.name}
+                        </a>
+                    ))}
+                    <a
+                        href="#discord"
+                        className="hidden md:flex items-center gap-2 px-4 py-2 text-white text-sm button-gradient rounded-[79px] hover:scale-110 transition-all duration-500"
+                    >
+                        <img src={discordIcon} alt="Discord Icon" className="w-6 h-6" />
+                        Discord
+                    </a>
+                </nav>
+
+                {/* Mobile Menu Button */}
+                <div className="flex lg:hidden">
+                    {!isMenuOpen && (
+                        <button
+                            className="text-white hover:text-gray-400"
+                            onClick={() => setIsMenuOpen(true)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={2}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M4 6h16M4 12h16m-7 6h7"
+                                />
+                            </svg>
+                        </button>
+                    )}
+                </div>
+            </div>
+
+            {/* Mobile Navigation */}
+            {isMenuOpen && (
+                <nav className="lg:hidden bg-black/80 min-h-screen w-full flex flex-col z-50 justify-center items-center text-center text-sm space-y-4 relative">
+                    {/* Close Button (Cross) at the top-right */}
+                    <div className="absolute top-4 right-4">
+                        <button onClick={() => setIsMenuOpen(false)} className="text-white hover:text-gray-400">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={2}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+
+                    {/* Menu Links */}
+                    {navLinks.map((link, index) => (
+                        <a
+                            key={index}
+                            href={link.href}
+                            className="block text-white hover:font-bold text-[18px] transition-all ease-in-out duration-300"
+                        >
+                            {link.name}
+                        </a>
+                    ))}
+
+                    {/* Discord Link */}
+                    <a
+                        href="#discord"
+                        className="inline-flex items-center gap-2 px-4 py-2 text-sm button-gradient rounded-[79px] hover:scale-110 transition-all duration-300"
+                    >
+                        <img src={discordIcon} alt="Discord Icon" className="w-6 h-6" />
+                        Discord
+                    </a>
+                </nav>
+            )}
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/utils/helper.js
@@ -0,0 +1,6 @@
+export const NAV_DATA_LINK = [
+    { title: 'Home', link: '#home' },
+    { title: 'Services', link: '#services' },
+    { title: 'Projects', link: '#projects' },
+    { title: 'About', link: '#about' },
+];
